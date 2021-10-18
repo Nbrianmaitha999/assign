f5bf327c0b6912e3b29f66cf0c1a6427a638077f
Nbrianmaitha999 <91789747+Nbrianmaitha999@users.noreply.github.com>
2021-10-18 16:38:58 +0300

    Create area and perimeter of a rectangle.c

---
+++ area and perimeter of a rectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    int l=20,w=15,perimeter,area;
+    perimeter=2*(l+w);
+    area=l*w;
+    printf("\n perimeter=%d", perimeter);
+    printf("\n area=%d",area);
+    return 0;
+}
