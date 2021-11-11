70c00113cdd808530db9c7300465b98d7307b886
Nbrianmaitha999 <91789747+Nbrianmaitha999@users.noreply.github.com>
2021-11-11 15:41:43 +0300

    Create if....else.c

---
+++ if....else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int  n;
+    printf("enter value of a\n");
+    scanf("%d",&n);
+    if(n>20)
+    {
+      printf("n is grater than 5");
+     }
+   else{printf("n is not");}
+    return 0;
+}
